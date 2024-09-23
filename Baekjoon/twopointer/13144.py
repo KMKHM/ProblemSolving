d3caaa09fe436131964ec70bcfe2e60862785949
KMK_HM <kyun9151@gmail.com>
2024-09-23 15:58:37 +0900

    출석체크

--- Baekjoon/twopointer/13144.py
+++ Baekjoon/twopointer/13144.py
@@ -3,17 +3,22 @@ List of Unique Numbers
 문제: https://www.acmicpc.net/problem/13144
 """
 n = int(input())
+arr = list(map(int, input().split()))
 
-ans = n
+s = e = cnt = 0
+num_dict = {}
 
-nums = list(map(int, input().split()))
+while e < n:
+    print(num_dict)
+    print(s, e)
+    print(cnt)
+    if arr[e] not in num_dict:
+        num_dict[arr[e]] = 1
+        e += 1
+        cnt += e - s
+    else:
+        del num_dict[arr[s]]
+        s += 1
 
-for i in range(n-1):
-    start, end = i, i+1
-    while end < n:
-        if nums[start] == nums[end]:
-            break
-        else:
-            end += 1
-            ans += 1
-print(ans)
+print(cnt)
+
